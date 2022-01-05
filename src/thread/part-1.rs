e7bdb0a0b8d130995fa094599d87bbd210b91455
namleyolo <namhoangle1996@gmail.com>
2022-01-05 23:17:42 +0700

    update thread

--- src/thread/part-1.rs
+++ src/thread/part-1.rs
@@ -1,48 +1,67 @@
 use std::thread;
+use std::time::{Duration, Instant};
+
 
 pub fn example() {
-    // ex1();
-    // ex2();
-    ex3();
+    // ex();
+    ex1();
+    ex2();
+    // ex3();
+
+    thread::sleep(Duration::from_secs(2));
 
 }
 
-fn ex1() {
-    thread::spawn(|| {
-        for i in 1..19 {
-            println!("hi number {} from the spawned thread!", i);
-        }
-    });
+fn ex() {
+    let start = Instant::now();
 
     for i in 1..5 {
-        println!("hi number {} from the main thread!", i);
+        println!("hi number {} from the 1st loop !", i);
+        thread::sleep(Duration::from_secs(1));
     }
+
+    for i in 1..5 {
+        println!("hi number {} from the 2nd loop !", i);
+        thread::sleep(Duration::from_secs(1));
+    }
+
+    let duration = start.elapsed();
+    println!("---------Time spent : {:?}", duration);
+}
+
+
+fn ex1() {
+    let handle = thread::spawn(|| {
+        for i in 1..1000 {
+            println!("hi number {} from the spawned thread ex1 !", i);
+        }
+    });
 }
 
 
 fn ex2() {
     let handle = thread::spawn(|| {
         for i in 1..10 {
-            println!("hi number {} from the spawned thread!", i);
-            // thread::sleep(Duration::from_millis(1));
+            println!("hi number {} from the spawned thread ex2 !", i);
         }
     });
-
-    handle.join().unwrap();
-    // => Thread main sẽ đợi thread được sinh ra thực hiện xong
-    for i in 1..5 {
-        println!("hi number {} from the main thread!", i);
-        // thread::sleep(Duration::from_millis(1));
-    }
 }
 
 
 fn ex3() {
-    let v = vec![1, 2, 3];
+    let mut x = "a string";
 
     let handle = thread::spawn(move || {
-        println!("Here's a vector: {:?}", v);
+        let x = "test1";
+        println!(" value of x: {:?}", x);
+    });
+
+    let handle2 = thread::spawn(move || {
+        let x = "test222";
+        println!(" value of x: {:?}", x);
     });
 
     handle.join().unwrap();
+
+    println!("value of x : {:?}", x)
 }
