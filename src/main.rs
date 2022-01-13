c2ae956743d362ee040187af054feca1e630f736
namleyolo <namhoangle1996@gmail.com>
2022-01-13 23:20:40 +0700

    serve http server in Parallel

--- src/main.rs
+++ src/main.rs
@@ -1,157 +1,49 @@
-#[path = "./structure.rs"]
-mod custom_type;
-
-#[path = "./enum.rs"]
-mod enum_example;
-
-#[path = "./constant.rs"]
-mod constant;
-
-#[path = "./mutability.rs"]
-mod mutability;
-
-#[path = "./scopeAndShadowing.rs"]
-mod scope_and_shadowing;
-
-#[path = "./casting.rs"]
-mod casting;
-
-#[path = "./literal.rs"]
-mod literal;
-
-#[path = "./inference.rs"]
-mod inference;
-
-#[path = "./fromAndInto.rs"]
-mod from_and_into;
-
-#[path = "./tryFromAndInto.rs"]
-mod try_from_and_into;
-
-#[path = "./flowOfControl/if_else.rs"]
-mod if_else;
-
-#[path = "./flowOfControl/loop.rs"]
-mod loop_ex;
-
-#[path = "./flowOfControl/loop_returning.rs"]
-mod loop_returning;
-
-#[path = "./flowOfControl/while.rs"]
-mod while_ex;
-
-#[path = "./flowOfControl/for.rs"]
-mod for_ex;
-
-#[path = "./flowOfControl/match.rs"]
-mod match_ex;
-
-#[path = "./flowOfControl/if_let.rs"]
-mod if_let;
-
-#[path = "./flowOfControl/while_let.rs"]
-mod while_let;
-
-#[path = "./function/index.rs"]
-mod function_main;
-
-#[path = "./function/method.rs"]
-mod method;
-
-#[path = "./function/closures/closures.rs"]
-mod closures;
-
-#[path = "./function/closures/capturing.rs"]
-mod closures_capturing;
-
-#[path = "./thread/part-1.rs"]
-mod thread_part1;
-#[path = "./thread/part-2.rs"]
-mod thread_part2;
-#[path = "./thread/part-3.rs"]
-mod thread_part3;
-
-
+use std::fs;
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
-    // tuples();
-    // array();
-    // slice();
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
-    // enum_example::enum_example();
-    // constant::constant();
-    // mutability::example();
+    // Block forever, handling each request that arrives at this IP address
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
 
-    // scope_and_shadowing::scope();
-
-    // casting::example();
-    // literal::example();
-    // inference::example();
-    // from_and_into::example();
-    // try_from_and_into::example();
-
-    // if_else::example();
-    // loop_ex::example();
-    // loop_returning::example();
-    // while_ex::example();
-    // for_ex::example();
-    // if_let::example();
-    // while_let::example();
-
-
-    // function_main::example();
-    // method::example();
-    // closures::example();
-    // closures_capturing::example();
-
-    // thread_part1::example();
-    // thread_part2::example();
-    thread_part3::example();
+        handle_connection(stream);
+    }
 }
 
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    let x = stream.read(&mut buffer);
 
-fn tuples() {
-    //a collection that has diff types
-    let long_tuple = (1u8, 2u16, 3u32, 4u64,
-                      -1i8, -2i16, -3i32, -4i64,
-                      0.1f32, 0.2f64,
-                      'a', true);
+    if let Err(e) = x {
+        println!("{:?}", e);
+        return;
+    };
+    x.unwrap();
 
-    // get value of a tuple's index
-    println!("long tuple 8's value: {}", long_tuple.8);
-    //get value of the tuple
-    println!(" tuples: {:?}", long_tuple);
+    let get = b"GET / HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
+    let contents = fs::read_to_string(filename).unwrap();
+
+    // Write response back to the stream,
+    // and flush the stream to ensure the response is sent back to the client
+    let response = format!("{}{}", status_line, contents);
 
 
-    // destructuring tuple
-    let tuple = (1, "hello", 4.5, true);
-    let (a, b, c, d) = tuple;
-    println!("get value of a {:?}", a);
+    let y = stream.write(response.as_bytes());
+    if let Err(e) = y {
+        println!("{:?}", e);
+        return;
+    };
+    y.unwrap();
+
+
+    stream.flush().unwrap();
 }
-
-fn array() {
-    //  has an array of length
-    let array: &[u8; 3] = &[1u8, 2, 3];
-    println!("{:?}", array);
-
-    //get value of array index
-    println!(" array 2: {:}", array[2]);
-}
-
-fn slice() {
-    //  has unsized
-    let slice: &[u8] = &[1u8, 2, 3];
-    println!("slice{:?}", slice);
-
-    //get value of array index
-    println!(" slice 2: {:}", slice[1]);
-}
-
-
-
-
-
-
-
-
-
